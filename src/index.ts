c8623803f1e9009c5cb67c76fecefc3293be8a29
jimao <jimao@jimaodeMacBook-Pro.local>
2023-07-05 15:50:51 +0800

    性能测试

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,22 @@
 import cluster from 'cluster';
 
+let old_time = 0;
+
+function loop(func: () => void, i = 0) {
+  if (i === 10000) {
+    console.log(Number(new Date()) - old_time);
+    return;
+  }
+  func();
+  // setImmediate(() => loop(func, ++i));
+  // process.nextTick(() => loop(func, ++i));
+  setTimeout(() => loop(func, ++i));
+}
+
 function main() {
-  
+  let i = 0;
+  old_time = Number(new Date());
+  loop(() => i++);
 }
 
 main();
