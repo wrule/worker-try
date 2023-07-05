3ab64926b36a560b7416efc529e98e687e6f1a35
jimao <jimao@jimaodeMacBook-Pro.local>
2023-07-05 14:04:04 +0800

    清空

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,7 @@
 import cluster from 'cluster';
 
 function main() {
-  const func = (data: any) => {
-    console.log('主进程接收', data);
-  };
-
-  if (cluster.isMaster) {
-    console.log('主进程', process.pid);
-    const workers = Array(2).fill(0).map(() => cluster.fork());
-    workers.forEach((worker) => worker.on('message', func));
-  } else {
-    console.log('子进程', process.pid);
-    setInterval(() => {
-      console.log('子进程发送', process.pid);
-      process.send?.(process.pid);
-    }, 2000);
-  }
+  
 }
 
 main();
